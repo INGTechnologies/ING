27d231784bf451010a0291220e8f974ab1ed1bc5
n-c0d3r <dntechunity@gmail.com>
2021-12-19 16:12:34 +0700

    init ING Engine in INGEditor\Source\Main.cpp

--- ING/ING/INGEditor/Source/Main.cpp
+++ ING/ING/INGEditor/Source/Main.cpp
@@ -6,7 +6,21 @@
 
 
 
+/**
+ *	Include Application
+ */
+#include <ING\Application\Application.h>
+
+
+
 int main() {
+
+
+
+	ING_CREATE_APPLICATION
+	ING_INIT_APPLICATION
+	ING_RUN_APPLICATION
+
 
 
 	system("pause");
