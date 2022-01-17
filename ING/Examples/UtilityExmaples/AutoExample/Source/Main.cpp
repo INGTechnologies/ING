0110d57cebf6cd0e334d9403514793ba064907d6
n-c0d3r <dntechunity@gmail.com>
2022-01-17 11:07:43 +0700

    add AutoExample project.

---
+++ ING/Examples/UtilityExmaples/AutoExample/Source/Main.cpp
@@ -0,0 +1,88 @@
+
+/**
+ *	Include IOStream
+ */
+#include <iostream>
+
+
+
+/**
+ *	Include Application
+ */
+#include <ING/Application/Application.h>
+
+
+
+/**
+ *	Include Thread And Thread Manager
+ */
+#include <ING/Thread/Thread.h>
+
+#include <ING/Thread/Manager/Manager.h>
+
+
+
+/**
+ *	Include Event And Event Manager
+ */
+#include <ING/Event/Event.h>
+
+#include <ING/Event/Manager/Manager.h>
+
+
+
+/**
+ *	Include Rendering
+ */
+#include <ING/Rendering/Rendering.h>
+
+using namespace ING::Rendering;
+
+
+
+/**
+ *	Include Utils
+ */
+#include <ING/Utils/Utils.h>
+
+using namespace ING::Utils;
+
+
+
+/**
+ *	Include Debug
+ */
+#include <ING/_Debug/Debug.h>
+
+
+
+using namespace ING;
+
+
+
+int main() {
+
+
+
+	//Create ING Application
+	ING::Application::CreateInstance();
+
+	ING::Application::GetInstance()->Init();
+
+	ING::Application::GetInstance()->Run();
+
+
+
+	Auto a = 5;
+
+	Debug::Log(a.As<int>());
+
+	a = std::string("Hello World");
+
+	Debug::Log(a.As<std::string>());
+
+
+
+	system("pause");
+	return 0;
+}
