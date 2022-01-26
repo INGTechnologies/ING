a1af5ac610e6f6e35ce22f8aa134d92d50c19b63
n-c0d3r <dntechunity@gmail.com>
2022-01-26 09:44:51 +0700

    add ConfigurationCompiler project and reconfig solution.

---
+++ ING/ING/INGSDK/ConfigurationCompiler/Lib/Source/ING/SDK/Project/Project.h
@@ -0,0 +1,27 @@
+#pragma once
+
+/**
+ *	Include Entry Point
+ */
+#include <ING/SDK/EntryPoint/EntryPoint.h>
+
+
+
+namespace ING {
+
+	namespace SDK {
+
+		class ING_SDK_API Project {
+
+			/**
+			 *	Constructors And Destructor
+			 */
+		public:
+			Project();
+			~Project();
+
+		};
+
+	}
+
+}
