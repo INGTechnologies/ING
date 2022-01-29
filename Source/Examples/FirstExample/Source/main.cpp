f0f2050c7b3612cf21a5373ba7a741dd8a7bb7f8
n-c0d3r <dntechunity@gmail.com>
2022-01-29 11:02:06 +0700

    reconfig solution and remove example projects

--- Source/Examples/FirstExample/Source/main.cpp
+++ Source/Examples/FirstExample/Source/main.cpp
@@ -91,6 +91,13 @@ using namespace ING::Utils;
 
 
 
+/**
+ *	Include Configuration
+ */
+#include <ING/Configuration/Configuration.h> 
+
+
+
 
 using namespace ING;
 
@@ -103,11 +110,27 @@ int main() {
 	//Create ING Application
 	ING::Application::CreateInstance();
 
+
+
+	APPLICATION_SET_CONFIG_PROP(bool, "ING::WindowManager::showConsoleWindow", true);
+
+
+
 	ING::Application::GetInstance()->Init();
+
+
 
 	Window* mainWindow = WindowManager::GetInstance()->GetMainWindow();
 
 	mainWindow->SetTitle(L"First ING Application");
+
+
+
+	Window* consoleWindow = WindowManager::GetInstance()->GetConsoleWindow();
+
+	consoleWindow->SetTitle(L"First ING Console");
+
+
 
 	ING::Application::GetInstance()->Run();
 
