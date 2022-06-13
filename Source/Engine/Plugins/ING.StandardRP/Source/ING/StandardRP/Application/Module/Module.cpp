12bb6fb248d92096c7fe2041b482fac3dd7aee44
n-c0d3r <dntechunity@gmail.com>
2022-06-13 22:13:29 +0700

    add some default passes

--- Source/Engine/Plugins/ING.StandardRP/Source/ING/StandardRP/Application/Module/Module.cpp
+++ Source/Engine/Plugins/ING.StandardRP/Source/ING/StandardRP/Application/Module/Module.cpp
@@ -20,6 +20,20 @@
 
 
 
+/**
+ *	Include StandardRP Passes
+ */
+#include <ING/StandardRP/Pass/DepthPrepass/DepthPrepass.h>
+#include <ING/StandardRP/Pass/FinalPass/FinalPass.h>
+#include <ING/StandardRP/Pass/FirstPass/FirstPass.h>
+#include <ING/StandardRP/Pass/GBufferPass/GBufferPass.h>
+#include <ING/StandardRP/Pass/PostProcessPass/PostProcessPass.h>
+#include <ING/StandardRP/Pass/SkyPass/SkyPass.h>
+#include <ING/StandardRP/Pass/TransparentPass/TransparentPass.h>
+#include <ING/StandardRP/Pass/UIPass/UIPass.h>
+
+
+
 namespace ING {
 
 	namespace StandardRP {
@@ -33,6 +47,15 @@ namespace ING {
 
 			RegisterType<C_Pipeline>(0);
 			RegisterType<C_Renderer>(0);
+
+			RegisterType<C_DepthPrepass>(0);
+			RegisterType<C_FinalPass>(0);
+			RegisterType<C_FirstPass>(0);
+			RegisterType<C_GBufferPass>(0);
+			RegisterType<C_PostProcessPass>(0);
+			RegisterType<C_SkyPass>(0);
+			RegisterType<C_TransparentPass>(0);
+			RegisterType<C_UIPass>(0);
 
 
 
---
+++ Source/Engine/Plugins/ING.StandardRP/Source/ING/StandardRP/Pass/PostProcessPass/PostProcessPass.h
@@ -0,0 +1,63 @@
+#pragma once
+
+/**
+ *	Include StandardRP Plugin
+ */
+#include <ING\StandardRP\Plugin\Plugin.h>
+
+
+
+/**
+ *	Include Utils
+ */
+#include <ING/Utils/Utils.h>
+
+using namespace ING::Utils;
+
+
+
+/**
+ *	Include Rendering Pass
+ */
+#include <ING/Rendering/Pass/Pass.h>
+
+
+
+namespace ING {
+
+	namespace StandardRP {
+
+		class PLUGIN_API(ING_STANDARDRP) C_PostProcessPass : public Rendering::C_Pass
+		{
+
+			ING_REFLECT_CLASS(C_PostProcessPass, Rendering::C_Pass)
+
+
+
+			/**
+			 *	Constructor
+			 */
+		protected:
+			void Constructor(const String& name);
+
+
+
+			/**
+			 *	Release Methods
+			 */
+		public:
+			virtual void Release() override;
+
+
+
+			/**
+			 *	Methods
+			 */
+		public:
+			virtual bool Render(Rendering::IDeviceContext * context, C_Camera * camera) override;
+
+		};
+
+	}
+
+}
